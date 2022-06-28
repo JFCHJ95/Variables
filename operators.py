94d94db76de8a05926b45ad9152a72d137e274ad
Jonathan <chavez.jonathan5@yahoo.es>
2022-06-28 11:27:54 -0500

    operator

--- operators.py
+++ operators.py
@@ -1,5 +1,32 @@
 # Basic python operators
 
-# Input
-var1 = input('Message: ')
+# Funtion Input
+#var1 = input('Message: ')
 
+# Operators
+var2: int = 5
+var3: int = 4
+
+# Equal ==
+if var2 == var3:
+    print("Equal")
+
+# Not equal !=
+if var2 != var3:
+    print("Not equal")
+
+# Greater than >
+if var2 > var3:
+    print("Is greater")
+
+# Less than <
+if var2 < var3:
+    print("Is less")
+
+# Greater than or equal to >=
+if var2 >= var3:
+    print("Is greater than or equal to")
+
+# Less than or equal <=
+if var2 <= var3:
+    print("Is less than or equal to")
