916cbaab9b36be100828e1598b9c96862f130aea
Jonathan <chavez.jonathan5@yahoo.es>
2022-06-28 13:42:15 -0500

    change

--- cando.py
+++ cando.py
@@ -1,18 +1,25 @@
 # Python Classes and Objects
 
+from inspect import _void
+
+
 class Person:
-  def __init__(self, name, age):
+  def __init__(self, name: str, age: int) -> _void:
     """
     This is a funtion defines the parameters of the PERSON class
     """
-    self.name = name
-    self.age = age
+    self.name: str = name
+    self.age: int = age
 
-  def sayname(self):
+  def say_name(self) -> _void:
     """
     This is a funtion that prints your name
     """
     print("Hello my name is " + self.name + " and I have " + str(self.age) + " years old")
 
-p1 = Person("Jonathan", 26)
-p1.sayname()
+dario: Person = Person("Dario", 26)
+jonathan: Person = Person("Jonathan", 26)
+people: list[Person] = [jonathan, dario] # Array 
+
+for person in people:
+    person.say_name()
--- variables.py
+++ variables.py
@@ -13,6 +13,16 @@ var5: bool = True
 var6: bool = False
 
 # Secuence
-var7: tuple = (0, 1, 2, 3)
-var8: list = [0, 1, 2, 3]
-var9: set = {0, 1, 2, 3}
+var7: tuple[int] = (0, 1, 2, 3)
+var8: list[int] = [0, 1, 2, 3]
+var9: set[int] = {0, 1, 2, 3}
+
+list_names: list[str] = ["Jonathan", "Dario", "Gema", "Ivan", "Ramiro"]
+
+jonathan_age: int = 25
+def calculate_is_legal (age: int) -> bool:
+    return age >= 18
+
+is_jonathan_legal: bool = calculate_is_legal(age=jonathan_age)
+print("is_jonathan_legal: ", is_jonathan_legal) # 
+print(f"is_jonathan_legal: {is_jonathan_legal}") # Concatena 2 strings
