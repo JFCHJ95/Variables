af764f945961ee3923683d7804a56ed25185c1fd
Jonathan <chavez.jonathan5@yahoo.es>
2022-06-28 11:03:43 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Types of variables in Python
+
+# String
+var1: str = "Hi world"
+
+# Numeric
+var2: int = 25
+var3: float = 25.5
+var4: complex = 25+5j
+
+# Boolean
+var5: bool = True
+var6: bool = False
+
+# Secuence
+var7: tuple = (0, 1, 2, 3)
+var8: list = [0, 1, 2, 3]
+var9: set = {0, 1, 2, 3}
